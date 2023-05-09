ac2c54418a9e22747ec790620a1040c649e140f3
eddiemendez95 <106997009+eddiemendez95@users.noreply.github.com>
2023-05-09 13:29:07 -0700

    post coding session review

--- controllers/clothingitem.js
+++ controllers/clothingitem.js
@@ -5,7 +5,6 @@ const createItem = (req, res) => {
   console.log(req.body);
 
   const { name, weather, imageUrl } = req.body;
-
   ClothingItem.create({ name, weather, imageUrl })
     .then((item) => {
       console.log(item);
@@ -16,4 +15,35 @@ const createItem = (req, res) => {
     });
 };
 
-module.exports = { createItem };
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from getItems", e });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageUrl } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from updateItem", e });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from deleteItem", e });
+    });
+};
+
+module.exports = { createItem, getItems, updateItem, deleteItem };
--- routes/clothingitem.js
+++ routes/clothingitem.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-const { createItem } = require("../controllers/clothingitem");
+const {
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem,
+} = require("../controllers/clothingitem");
 
 router.post("/", createItem);
+router.get("/", getItems);
+router.put("/:itemId", updateItem);
+router.delete("/:itemId", deleteItem);
 
 module.exports = router;
