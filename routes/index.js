0ee35f478c1253ffeaac73122b77be7ff9e52ec7
eddiemendez95 <106997009+eddiemendez95@users.noreply.github.com>
2023-05-11 14:17:37 -0700

    add to index js route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const user = require("../models/user");
 const clothingItem = require("./clothingitem");
 const users = require("./users");
 
 router.use("/items", clothingItem);
-router.use("/users");
+router.use("/users", users);
 
 router.use((req, res) => {
   res.status(500).send({ message: "Router not found" });
